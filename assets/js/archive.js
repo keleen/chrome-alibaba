fa33208dabe4f8378aa7583b0d96ad899f06ac52
xiaoda.zhang2 <xiaoda.zhang2@easystack.cn>
2020-09-07 16:30:29 +0800

    1. 完成下载打包

--- assets/js/archive.js
+++ assets/js/archive.js
@@ -2,8 +2,7 @@
 
 ;;
 (function () {
- 
-  var zip = new JSZip();
+
   var productId = $('meta[name="b2c_auction"]').attr('content');
 
   function saveAs(content, fileName) {
@@ -15,6 +14,10 @@
     a.remove();
   }
 
+  /**
+   * 获取 URL 中的文件名称
+   * @param {*} url 
+   */
   function getFileName(url) {
     if (url) {
       var arr = url.split('/')
@@ -24,6 +27,10 @@
     }
   }
 
+  /**
+   * 获取文件名的扩展名
+   * @param {*} fileName 
+   */
   function getFileExtName(fileName) {
     if (fileName) {
       var arr = fileName.split('.');
@@ -33,49 +40,142 @@
     }
   }
 
-  function getBase64(imgUrl) {
+  /**
+   * 把一个网络上的图片转换成 blob 对象 
+   * @param {}} imgUrl 
+   */
+  function getXhrBase64(imgUrl) {
     window.URL = window.URL || window.webkitURL;
     var xhr = new XMLHttpRequest();
-    xhr.open("get", imgUrl, true);
-    // 至关重要
+    xhr.responseType = "blob";
     return new Promise(function (resolve, reject) {
-      xhr.responseType = "blob";
       xhr.onload = function () {
-        if (this.status == 200) {
+        if (this.status === 200) {
           resolve(this.response);
-        } else {
-          reject('error')
         }
       }
+      xhr.onreadystatechange = function () {}
+      xhr.onerror = function () {
+        reject('error')
+      }
+      xhr.open("get", imgUrl, true);
       xhr.send();
     });
   }
 
+  function errorPromise () {
+    return new Promise((resolve, reject) => {
+      resolve('error');
+    });
+  }
+
+  function getBase64Image(img, width, height) {
+    var canvas = document.createElement("canvas");
+    canvas.width = width ? width : img.width;
+    canvas.height = height ? height : img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    var dataURL = canvas.toDataURL();
+    return dataURL;
+  }
+
+  function getCanvasBase64(img) {
+    try {
+      if (img) {
+        var image = new Image();
+        //至关重要
+        image.crossOrigin = '';
+        image.src = img;
+        //至关重要
+        var deferred = $.Deferred();
+        image.onerror = function () {
+          console.log("img.error")
+        };
+        image.onabort = function () {
+          console.log("img.abort")
+        };
+        image.onload = function () {
+          console.log(image);
+          deferred.resolve(getBase64Image(image)); //将base64传给done上传处理
+          //document.getElementById("container2").appendChild(image);
+        }
+        return deferred.promise(); //问题要让onload完成后再return sessionStorage['imgTest']
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  /**
+   * 获取阿里巴巴网页对应的商品头图、商品详情等素材图片
+   */
   function getImgURL() {
-    var files = {};
+    var files = [];
     $('.nav.nav-tabs.fd-clr li').each(function (index, ele) {
       var imgs = $(ele).data('imgs');
-      var fileName = getFileName(imgs.original);
-      var fileExtName = getFileName(fileExtName);
-      var realFileName = `${productId}_banner_${index}.${fileExtName}`;
-      // banner.push(imgs.original);
-      files[realFileName] = imgs.original;
+      if (imgs) {
+        var fileName = getFileName(imgs.original);
+        var fileExtName = getFileExtName(fileName);
+        var realFileName = `${productId}_banner_${index}.${fileExtName}`;
+        files.push({
+          url: imgs.original,
+          filename: realFileName
+        });
+      }
     });
 
     $('#mod-detail-description img').each(function (index, ele) {
       var src = $(ele).attr('src');
-      detail.push(src);
+      if (src) {
+        var fileName = getFileName(src);
+        var fileExtName = getFileExtName(fileName);
+        var realFileName = `${productId}_description_${index}.${fileExtName}`;
+        files.push({
+          url: src,
+          filename: realFileName
+        });
+      }
     });
+
+    return files;
+  }
+
+  function packageZip(files) {
+    var zip = new JSZip();
+    for (var i = 0; i < files.length; i++) {
+      zip.file(files[i].filename, files[i].blob);
+    }
+    zip.generateAsync({
+      type: 'blob'
+    }).then(function (content) {
+      saveAs(content, `${productId}.zip`);
+    })
   }
 
   function start() {
-    getin
+    var files = getImgURL();
+    var promiseArr = [];
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      try {
+        var p = getXhrBase64(file.url)
+        promiseArr.push(p);
+      } catch (e) {
+        // promiseArr.push(errorPromise())
+        console.log('start  promise error');
+      }
+
+    }
+
+    Promise.all(promiseArr).then(function (ret) {
+      for (var i = 0; i < ret.length; i++) {
+        files[i].blob = ret[i];
+      }
+      packageZip(files);
+    });
   }
 
-
-
-
-
-
+  start();
 
 })();
